be4d14aac7cdf629c301db33d2fd675a7a2a63d7
林 真希 <fko2247051@stu.o-hara.ac.jp>
2022-10-21 10:29:51 +0900

    敵の組み込み グラフィックの反転表示

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -19,6 +19,8 @@ void SceneMain::init()
 
 	m_player.setHandle(m_hPlayerGraphic);
 	m_player.init();
+	m_enemy.setHandle(m_hPlayerGraphic);
+	m_enemy.init();
 
 	for (auto& shot : m_shot)
 	{
@@ -37,6 +39,7 @@ void SceneMain::end()
 void SceneMain::update()
 {
 	m_player.update();
+	m_enemy.update();
 	for (auto& shot : m_shot)
 	{
 		shot.update();
@@ -53,6 +56,7 @@ void SceneMain::update()
 void SceneMain::draw()
 {
 	m_player.draw();
+	m_enemy.draw();
 
 	for (auto& shot : m_shot)
 	{
